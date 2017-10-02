92156340f72ce8e50b536055d551cb3ec9bb2a45
Barbara <babsi.joebstl@gmail.com>
2017-10-02 13:18:05 +0200

    implement functions in my_planning_graph.py, add heuristic analysis

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -74,15 +74,12 @@ class AirCargoProblem(Problem):
                             expr("At({}, {})".format(cargo, airport))]
 
                         load = Action(
-                            expr("Load( {}, {}, {}".format(
-                                cargo, plane, airport)), [precond_pos, precond_neg], [effect_add, effect_rem]
-                        )
+                            expr("Load( {}, {}, {})".format(cargo, plane, airport)), [precond_pos, precond_neg], [effect_add, effect_rem])
                         loads.append(load)
             return loads
 
         def unload_actions():
             """Create all concrete Unload actions and return a list
-
             :return: list of Action objects
             """
             unloads = []
@@ -98,10 +95,10 @@ class AirCargoProblem(Problem):
                         effect_add = [
                             expr("At({}, {})".format(cargo, airport))]
                         effect_rem = [
-                            expr("In({}, {})".format(cargo, airport))]
+                            expr("In({}, {})".format(cargo, plane))]
 
                         unload = Action(
-                            expr("Unload( {}, {}, {}".format(
+                            expr("Unload( {}, {}, {})".format(
                                 cargo, plane, airport)), [precond_pos, precond_neg], [effect_add, effect_rem]
                         )
                         unloads.append(unload)
@@ -146,7 +143,6 @@ class AirCargoProblem(Problem):
 
         for action in self.actions_list:
             if (all(precond_pos in kb.clauses for precond_pos in action.precond_pos) and all(precond_neg not in kb.clauses for precond_neg in action.precond_neg)):
-
                 possible_actions.append(action)
 
         return possible_actions
@@ -160,9 +156,6 @@ class AirCargoProblem(Problem):
         :param action: Action applied
         :return: resulting state after action
         """
-        # TODO implement
-        print state
-        print action
 
         new_state = FluentState([], [])
         old_state = decode_state(state, self.state_map)
@@ -171,13 +164,13 @@ class AirCargoProblem(Problem):
             if fluent not in action.effect_rem:
                 new_state.pos.append(fluent)
 
-        for fluent in action.effect_add:
-            if fluent not in new_state.pos:
-                new_state.pos.append(fluent)
-
         for fluent in old_state.neg:
             if fluent not in action.effect_add:
                 new_state.neg.append(fluent)
+
+        for fluent in action.effect_add:
+            if fluent not in new_state.pos:
+                new_state.pos.append(fluent)
 
         for fluent in action.effect_rem:
             if fluent not in new_state.neg:
@@ -222,9 +215,15 @@ class AirCargoProblem(Problem):
         conditions by ignoring the preconditions required for an action to be
         executed.
         """
-        # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig
+        # implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig
         # Ed-2 11.2)
+
+        kb = PropKB()
+        kb.tell(decode_state(node.state, self.state_map).pos_sentence())
         count = 0
+        for clause in self.goal:
+            if clause not in kb.clauses:
+                count += 1
         return count
 
 
@@ -259,41 +258,41 @@ def air_cargo_p2() -> AirCargoProblem:
     airports = ['JFK', 'SFO', 'ATL']
 
     pos = [expr('At(C1, SFO)'),
-           expr'At(C2, JFK)'),
+           expr('At(C2, JFK)'),
            expr('At(C3, ATL)'),
            expr('At(P1, SFO)'),
            expr('At(P2, JFK)'),
            expr('At(P3, ATL)')]
 
     neg = [expr('At(C1, JFK)'),
-        expr('At(C1, ATL)'),
-        expr('In(C1, P1)'),
-        expr('In(C1, P2)'),
-        expr('In(C1, P3)'),
+           expr('At(C1, ATL)'),
+           expr('In(C1, P1)'),
+           expr('In(C1, P2)'),
+           expr('In(C1, P3)'),
 
-        expr('At(C2, SFO)'),
-        expr('At(C2, ATL)'),
-        expr('In(C2, P1)'),
-        expr('In(C2, P2)'),
-        expr('In(C2, P3)'),
+           expr('At(C2, SFO)'),
+           expr('At(C2, ATL)'),
+           expr('In(C2, P1)'),
+           expr('In(C2, P2)'),
+           expr('In(C2, P3)'),
 
-        expr('At(C3, SFO)'),
-        expr('At(C3, JFK)'),
-        expr('In(C3, P1)'),
-        expr('In(C3, P2)'),
-        expr('In(C3, P3)'),
+           expr('At(C3, SFO)'),
+           expr('At(C3, JFK)'),
+           expr('In(C3, P1)'),
+           expr('In(C3, P2)'),
+           expr('In(C3, P3)'),
 
-        expr('At(P1, JFK)'),
-        expr('At(P1, ATL)'),
-        expr('At(P2, ATL)'),
-        expr('At(P2, SFO)'),
-        expr('At(P3, JFK)'),
-        expr('At(P3, ATL)')]
+           expr('At(P1, JFK)'),
+           expr('At(P1, ATL)'),
+           expr('At(P2, ATL)'),
+           expr('At(P2, SFO)'),
+           expr('At(P3, JFK)'),
+           expr('At(P3, SFO)')]
 
-        init = FluentState(pos, neg)
-        goal = [expr('At(C1, JFK)'),
-                expr('At(C2, SFO)'),
-                expr('At(C3, SFO)')]
+    init = FluentState(pos, neg)
+    goal = [expr('At(C1, JFK)'),
+            expr('At(C2, SFO)'),
+            expr('At(C3, SFO)')]
 
     return AirCargoProblem(cargos, planes, airports, init, goal)
 
@@ -304,7 +303,7 @@ def air_cargo_p3() -> AirCargoProblem:
     airports = ['JFK', 'SFO', 'ATL', 'ORD']
 
     pos = [expr('At(C1, SFO)'),
-           expr'At(C2, JFK)'),
+           expr('At(C2, JFK)'),
            expr('At(C3, ATL)'),
            expr('At(C4, ORD)'),
            expr('At(P1, SFO)'),
@@ -312,41 +311,40 @@ def air_cargo_p3() -> AirCargoProblem:
 
     neg = [expr('At(C1, JFK)'),
            expr('At(C1, ATL)'),
-           expr('At(C1, ORD'),
+           expr('At(C1, ORD)'),
            expr('In(C1, P1)'),
            expr('In(C1, P2)'),
 
-            expr('At(C2, SFO)'),
-            expr('At(C2, ATL)'),
-            expr('At(C2, ORD)'),
-            expr('In(C2, P1)'),
-            expr('In(C2, P2)'),
+           expr('At(C2, SFO)'),
+           expr('At(C2, ATL)'),
+           expr('At(C2, ORD)'),
+           expr('In(C2, P1)'),
+           expr('In(C2, P2)'),
 
-            expr('At(C3, SFO)'),
+           expr('At(C3, SFO)'),
+           expr('At(C3, JFK)'),
+           expr('At(C3, ORD)'),
+           expr('In(C3, P1)'),
+           expr('In(C3, P2)'),
+
+           expr('At(C4, SFO)'),
+           expr('At(C4, JFK)'),
+           expr('At(C4, ATL)'),
+           expr('In(C4, P1)'),
+           expr('In(C4, P2)'),
+
+           expr('At(P1, JFK)'),
+           expr('At(P1, ATL)'),
+           expr('At(P1, ORD)'),
+           expr('At(P2, ATL)'),
+           expr('At(P2, SFO)'),
+           expr('At(P2, ORD)')
+           ]
+
+    init = FluentState(pos, neg)
+    goal = [expr('At(C1, JFK)'),
             expr('At(C3, JFK)'),
-            expr('At(C3, ORD)'),
-            expr('In(C3, P1)'),
-            expr('In(C3, P2)'),
-
-            expr('At(C4, SFO)'),
-            expr('At(C4, JFK)'),
-            expr('At(C4, ATL)'),
-            expr('In(C4 P1)'),
-            expr('In(C4, P2)'),
-
-            expr('At(P1, JFK)'),
-            expr('At(P1, ATL)'),
-            expr('At(P1, ORD)'),
-            expr('At(P2, ATL)'),
-            expr('At(P2, SFO)'),
-            expr('At(P2, ORD)')
-            ]
-
-        init = FluentState(pos, neg)
-        goal = [expr('At(C1, JFK)'),
-                expr('At(C3, JFK'),
-                expr('At(C2, SFO)'),
-                expr('At(C4, SFO')]
+            expr('At(C2, SFO)'),
+            expr('At(C4, SFO)')]
 
     return AirCargoProblem(cargos, planes, airports, init, goal)
-   
--- my_planning_graph.py
+++ my_planning_graph.py
@@ -105,7 +105,6 @@ class PgNode_s(PgNode):
 class PgNode_a(PgNode):
     """A-type (action) Planning Graph node - inherited from PgNode """
 
-
     def __init__(self, action: Action):
         """A-level Planning Graph node constructor
 
@@ -179,7 +178,8 @@ class PgNode_a(PgNode):
                 self.action.args == other.action.args)
 
     def __hash__(self):
-        self.__hash = self.__hash or hash(self.action.name) ^ hash(self.action.args)
+        self.__hash = self.__hash or hash(
+            self.action.name) ^ hash(self.action.args)
         return self.__hash
 
 
@@ -200,7 +200,7 @@ def mutexify(node1: PgNode, node2: PgNode):
 class PlanningGraph():
     """
     A planning graph as described in chapter 10 of the AIMA text. The planning
-    graph can be used to reason about 
+    graph can be used to reason about
     """
 
     def __init__(self, problem: Problem, state: str, serial_planning=True):
@@ -218,7 +218,8 @@ class PlanningGraph():
         self.problem = problem
         self.fs = decode_state(state, problem.state_map)
         self.serial = serial_planning
-        self.all_actions = self.problem.actions_list + self.noop_actions(self.problem.state_map)
+        self.all_actions = self.problem.actions_list + \
+            self.noop_actions(self.problem.state_map)
         self.s_levels = []
         self.a_levels = []
         self.create_graph()
@@ -246,9 +247,11 @@ class PlanningGraph():
         """
         action_list = []
         for fluent in literal_list:
-            act1 = Action(expr("Noop_pos({})".format(fluent)), ([fluent], []), ([fluent], []))
+            act1 = Action(expr("Noop_pos({})".format(fluent)),
+                          ([fluent], []), ([fluent], []))
             action_list.append(act1)
-            act2 = Action(expr("Noop_neg({})".format(fluent)), ([], [fluent]), ([], [fluent]))
+            act2 = Action(expr("Noop_neg({})".format(fluent)),
+                          ([], [fluent]), ([], [fluent]))
             action_list.append(act2)
         return action_list
 
@@ -274,7 +277,8 @@ class PlanningGraph():
         leveled = False
         level = 0
         self.s_levels.append(set())  # S0 set of s_nodes - empty to start
-        # for each fluent in the initial state, add the correct literal PgNode_s
+        # for each fluent in the initial state, add the correct literal
+        # PgNode_s
         for literal in self.fs.pos:
             self.s_levels[level].add(PgNode_s(literal, True))
         for literal in self.fs.neg:
@@ -303,13 +307,24 @@ class PlanningGraph():
         :return:
             adds A nodes to the current level in self.a_levels[level]
         """
-        # TODO add action A level to the planning graph as described in the Russell-Norvig text
+        # add action A level to the planning graph as described in the Russell-Norvig text
         # 1. determine what actions to add and create those PgNode_a objects
         # 2. connect the nodes to the previous S literal level
         # for example, the A0 level will iterate through all possible actions for the problem and add a PgNode_a to a_levels[0]
         #   set iff all prerequisite literals for the action hold in S0.  This can be accomplished by testing
         #   to see if a proposed PgNode_a has prenodes that are a subset of the previous S level.  Once an
-        #   action node is added, it MUST be connected to the S node instances in the appropriate s_level set.
+        # action node is added, it MUST be connected to the S node instances in
+        # the appropriate s_level set.
+        a_nodes = []
+        for a in self.all_actions:
+            a_node = PgNode_a(a)
+            if a_node.prenodes.issubset(self.s_levels[level]):
+                a_nodes.append(a_node)
+                for s_node in self.s_levels[level]:
+                    s_node.children.add(a_node)
+                    a_node.parents.add(s_node)
+
+        self.a_levels.append(a_nodes)
 
     def add_literal_level(self, level):
         """ add an S (literal) level to the Planning Graph
@@ -328,6 +343,14 @@ class PlanningGraph():
         #   may be "added" to the set without fear of duplication.  However, it is important to then correctly create and connect
         #   all of the new S nodes as children of all the A nodes that could produce them, and likewise add the A nodes to the
         #   parent sets of the S nodes
+
+        s_nodes = set()
+        for a_node in self.a_levels[level - 1]:
+            for s_node in a_node.effnodes:
+                s_nodes.add(s_node)
+                s_node.parents.add(a_node)
+                a_node.children.add(s_node)
+        self.s_levels.append(s_nodes)
 
     def update_a_mutex(self, nodeset):
         """ Determine and update sibling mutual exclusion for A-level nodes
@@ -385,7 +408,15 @@ class PlanningGraph():
         :param node_a2: PgNode_a
         :return: bool
         """
-        # TODO test for Inconsistent Effects between nodes
+        # test for Inconsistent Effects between nodes
+
+        for e in node_a1.action.effect_add:
+            if e in node_a2.action.effect_rem:
+                return True
+        for e in node_a2.action.effect_add:
+            if e in node_a1.action.effect_rem:
+                return True
+
         return False
 
     def interference_mutex(self, node_a1: PgNode_a, node_a2: PgNode_a) -> bool:
@@ -402,7 +433,21 @@ class PlanningGraph():
         :param node_a2: PgNode_a
         :return: bool
         """
-        # TODO test for Interference between nodes
+        # test for Interference between nodes
+
+        for e in node_a1.action.effect_add:
+            if e in node_a2.action.precond_neg:
+                return True
+        for e in node_a1.action.effect_rem:
+            if e in node_a2.action.precond_pos:
+                return True
+        for e in node_a2.action.effect_add:
+            if e in node_a1.action.precond_neg:
+                return True
+        for e in node_a2.action.effect_rem:
+            if e in node_a1.action.precond_pos:
+                return True
+
         return False
 
     def competing_needs_mutex(self, node_a1: PgNode_a, node_a2: PgNode_a) -> bool:
@@ -416,7 +461,13 @@ class PlanningGraph():
         :return: bool
         """
 
-        # TODO test for Competing Needs between nodes
+        # test for Competing Needs between nodes
+
+        for p_a1 in node_a1.parents:
+            for p_a2 in node_a2.parents:
+                if p_a1.is_mutex(p_a2):
+                    return True
+
         return False
 
     def update_s_mutex(self, nodeset: set):
@@ -451,7 +502,10 @@ class PlanningGraph():
         :param node_s2: PgNode_s
         :return: bool
         """
-        # TODO test for negation between nodes
+        # test for negation between nodes
+        if node_s1.symbol == node_s2.symbol and node_s1.is_pos != node_s2.is_pos:
+            return True
+
         return False
 
     def inconsistent_support_mutex(self, node_s1: PgNode_s, node_s2: PgNode_s):
@@ -470,15 +524,35 @@ class PlanningGraph():
         :param node_s2: PgNode_s
         :return: bool
         """
-        # TODO test for Inconsistent Support between nodes
-        return False
+        # test for Inconsistent Support between nodes
+        for p_s1 in node_s1.parents:
+            for p_s2 in node_s2.parents:
+                if not p_s1.is_mutex(p_s2):
+                    return False
+
+        return True
 
     def h_levelsum(self) -> int:
         """The sum of the level costs of the individual goals (admissible if goals independent)
 
         :return: int
         """
+        # implement
+        # for each goal in the problem, determine the level cost, then add them
+        # together
+
         level_sum = 0
-        # TODO implement
-        # for each goal in the problem, determine the level cost, then add them together
+        for goal in self.problem.goal:
+            is_goal = False
+            for depth in range(len(self.s_levels)):
+                for state in self.s_levels[depth]:
+                    literal = expr(state.symbol)
+                    if not state.is_pos:
+                        literal = '-' + literal
+                    if literal == goal:
+                        is_goal = True
+                        level_sum += depth
+                        break
+                if is_goal:
+                    break
         return level_sum
